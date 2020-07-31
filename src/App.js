839ec89bd30bdf68264dcce29fef2616c89856b0
Adam Friedman <adam@blok.party>
2020-07-31 10:47:22 -0700

    Added todos functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+function Todo({ todo, index }) {
+	return <div>{todo.text}</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([
+		{
+			text: 'Learn React',
+			isCompleted: false,
+		},
+		{
+			text: 'Learn Classes',
+			isCompleted: false,
+		},
+		{
+			text: 'Learn Hooks',
+			isCompleted: false,
+		},
+	]);
+
+	return (
+		<div>
+			<div>
+				{todos.map((todo, index) => (
+					<Todo key={index} index={index} todo={todo} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
